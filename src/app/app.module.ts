6567e2fcaa8e259ee90cfb16088a009703f2de84
monika <monika.bansal@nice.com>
2020-09-20 17:00:42 +0530

    server side rendering added and color changes for button and text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HttpClientModule} from '@angular/common/http';
     MainGridComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule
   ],
   providers: [SpaceXDataService],
